8640d57451ece8e641874e4ae3ec514c2ad3c9fb
Malte Reißig <malte@mikromedia.de>
2014-12-08 15:27:19 +0100

    Adapted to 4.4, module compiles and installs.
    Due to a missing moodle instance, functionality needs to be tested
    and most probably fixed regarding the following two API changes:
    * Transactions
    * loadChildTopics()
    
    Furthermore it would be nice to revise all exception handling of this plugin.

--- src/main/java/org/deepamehta/plugins/moodle/migrations/Migration5.java
+++ src/main/java/org/deepamehta/plugins/moodle/migrations/Migration5.java
@@ -3,46 +3,49 @@ package org.deepamehta.plugins.moodle.migrations;
 import de.deepamehta.core.RelatedTopic;
 import de.deepamehta.core.Topic;
 import de.deepamehta.core.model.SimpleValue;
-import de.deepamehta.core.service.Directives;
 import de.deepamehta.core.service.Migration;
 import de.deepamehta.core.service.ResultList;
 import java.util.logging.Logger;
 import org.deepamehta.plugins.moodle.MoodleServiceClient;
 
+/** 
+ * This migration removes the initially introduced Topic Types
+ * "Usage Report" and "Moodle Service URL" and changes the Workspace name to "Moodle".
+ */
 public class Migration5 extends Migration {
 
     private Logger logger = Logger.getLogger(getClass().getName());
-
+    
+    final String MOODLE_SERVICE_URL_TYPE =  "org.deepamehta.moodle.web_service_url";
+    final String USAGE_TYPE_URI = "org.deepamehta.moodle.usage_report";
+    
     @Override
     public void run() {
 
-        String usage_type_uri = "org.deepamehta.moodle.usage_report";
         // 1) Delete all instance of usage reports
-        ResultList<RelatedTopic> usage_reports = dms.getTopics(usage_type_uri, false, 0);
+        ResultList<RelatedTopic> usage_reports = dms.getTopics(USAGE_TYPE_URI, 0);
         for (int i=0; i < usage_reports.getItems().size(); i++) {
             RelatedTopic usage_report = usage_reports.getItems().get(i);
             logger.info("Migration 5 deleting \"Moodle Usage Report\"-Topic " + usage_report.getId());
             dms.deleteTopic(usage_report.getId());
         }
         // 2) Delete "Usage Report"-Type
-        dms.getTopicType(usage_type_uri).delete(new Directives());
+        dms.getTopicType(USAGE_TYPE_URI).delete();
         logger.info("Migration 5 deleted \"Moodle Usage Report\"-TopicType");
         //
-        String moodle_config_type_uri =  "org.deepamehta.moodle.web_service_url";
         // 3) Delete all instances of type to be deleted
-        ResultList<RelatedTopic> service_urls = dms.getTopics(moodle_config_type_uri, false, 0);
+        ResultList<RelatedTopic> service_urls = dms.getTopics(MOODLE_SERVICE_URL_TYPE, 0);
         for (int i=0; i < service_urls.getItems().size(); i++) {
             RelatedTopic service_url = service_urls.getItems().get(i);
             logger.info("Migration 5 deleting \"Moodle Web Service URL\"-Topic " + service_url.getId());
             dms.deleteTopic(service_url.getId());
         }
         // 4) Delete "Moodle Service URL"-Type
-        dms.getTopicType(moodle_config_type_uri).delete(new Directives());
+        dms.getTopicType(MOODLE_SERVICE_URL_TYPE).delete();
         // logger.info("Migration 5 deleted \"Moodle Web Service URL\"-TopicType");
         // 5) Fix Moodle WS-Name Topic
-        Topic moodle_ws = dms.getTopic("uri", new SimpleValue("org.deepamehta.workspaces.moodle"), true);
-            moodle_ws.getCompositeValue().set("dm4.workspaces.name", MoodleServiceClient.WS_MOODLE_NAME,
-                    null, new Directives());
+        Topic moodle_ws = dms.getTopic("uri", new SimpleValue("org.deepamehta.workspaces.moodle")).loadChildTopics();
+        moodle_ws.getChildTopics().set("dm4.workspaces.name", MoodleServiceClient.WS_MOODLE_NAME);
     }
 
 }
